267ac4173110e83c357e5a0849ca2185f16c1988
kororo11 <kororo11@naver.com>
2020-11-24 00:41:39 +0900

    Provider 컴포넌트를 사용하여 프로젝트에 리덕스 적용하기
    Provider를 사용할 때는 store 를 props 로 전달해 주어야 합니다.

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux';
 import rootReducer from './modules';
+import { Provider } from 'react-redux';
 
 const store = createStore(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root'),
+);
