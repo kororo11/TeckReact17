e4fca679155ae355b3916fdc0e08534f308de88b
kororo11 <kororo11@naver.com>
2020-11-24 01:02:19 +0900

    connect 함수 사용
    컴포넌트를 리덕스와 연동하기 위해서 react-redux 에서 제공하는 connect 함수를 사용 했습니다.
    mapStateToProps 는 리덕스 스토어 안의 상태를 컴포넌트의 props 로 넘겨주기 위해 설정하는 함수이고,
    mapDispatchToProps 는 액션 생성 함수를 컴포넌트의 props 로 넘겨주기 위해 사용하는 함수 입니다.

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import Counter from '../components/Counter';
+import { connect } from 'react-redux';
+import { decrease } from '../modules/counter';
 
-const CounterContainer = () => {
-	return <Counter />;
+const CounterContainer = ({ number, increase, decrease }) => {
+	return <Counter nu={number} onIncrease={increase} onDecrease={decrease} />;
 };
 
-export default CounterContainer;
+const mapStateToProps = state => ({
+	number: state.counter.number,
+});
+
+const mapDispatchToProps = dispatch => ({
+	// 임시함수
+	increase: () => {
+		console.log('increase');
+	},
+	decrease: () => {
+		console.log('decrease');
+	},
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
