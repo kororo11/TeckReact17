e90c2471593d00416bf41357d9af288df16bbc5c
kororo11 <kororo11@naver.com>
2020-11-24 00:38:34 +0900

    리액트 애플리케이션에 리덕스 적용하기
    드디어 리액트 애플리케이션에 리덕스를 적용합니다. 스토어를 만들고 리액트 애플리케이션에 리덕스를 적용하는 작업은 src 디렉터리의 index.js 에서 이루어 집니다.
    먼저 스토어 만들기

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import rootReducer from './modules';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root'),
-);
+const store = createStore(rootReducer);
+
+ReactDOM.render(<App />, document.getElementById('root'));
