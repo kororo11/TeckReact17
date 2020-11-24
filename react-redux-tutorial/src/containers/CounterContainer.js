23a11d8bd17acbbc651fa67936589b2c50cb238d
kororo11 <kororo11@naver.com>
2020-11-24 17:29:52 +0900

    useDespath Hook 적용하기

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Counter from '../components/Counter';
-import { connect } from 'react-redux';
 import { increase, decrease } from '../modules/counter';
-import { bindActionCreators } from 'redux';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 const CounterContainer = () => {
 	const number = useSelector(state => state.counter.number);
-	return <Counter number={number} />;
+	const dispatch = useDispatch();
+	const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+	const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+	return (
+		<Counter
+			number={number}
+			onIncrease={onIncrease}
+			onDecrease={onDecrease}
+		/>
+	);
 };
 
 export default CounterContainer;
