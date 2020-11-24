c9ac93c85b1ea936f8a1294ce772d5cfe2a6cca4
kororo11 <kororo11@naver.com>
2020-11-24 17:05:41 +0900

    payload 를 좀 더 간결하게
    모든 추가 데이터 값을 action.payload 로 사용하기 때문에 나중에 리듀서 코드를 다시 볼 때 헷갈릴 수 있습니다. 객체 비구조화 할당 문법으로 action 값의 payload 이름을 새로 설정해 주면 action.payload 가 정확히 어떤 값을 의미하는지 더 쉽게 파악할 수 있습니다.

--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -37,25 +37,23 @@ const initialState = {
 
 const todos = handleActions(
 	{
-		[CHANGE_INPUT]: (state, action) => ({
+		[CHANGE_INPUT]: (state, { payload: input }) => ({
 			...state,
-			input: action.payload,
+			input,
 		}),
-		[INSERT]: (state, action) => ({
+		[INSERT]: (state, { payload: todo }) => ({
 			...state,
-			todos: state.todos.concat(action.payload),
+			todos: state.todos.concat(todo),
 		}),
-		[TOGGLE]: (state, action) => ({
+		[TOGGLE]: (state, { payload: id }) => ({
 			...state,
 			todos: state.todos.map(todo =>
-				todo.id === action.payload
-					? { ...todo, done: !todo.done }
-					: todo,
+				todo.id === id ? { ...todo, done: !todo.done } : todo,
 			),
 		}),
-		[REMOVE]: (state, action) => ({
+		[REMOVE]: (state, { payload: id }) => ({
 			...state,
-			todos: state.todos.filter(todo => todo.id !== action.payload),
+			todos: state.todos.filter(todo => todo.id !== id),
 		}),
 	},
 	initialState,
